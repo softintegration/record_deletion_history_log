101583898af44ffb6b07320d12ec01d19fa7cfe4
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-10-30 10:19:05 +0100

    Record deletion history:Add the «Archiving» access right

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Record deletion history logs',
-    'version': '1.0.1',
+    'version': '1.0.1.1',
     'author':'Soft-integration',
     'category': 'Security/Access rights',
     'description': "",
