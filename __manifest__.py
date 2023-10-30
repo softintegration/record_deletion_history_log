8846e3fec2a0097483395bfcf0b2d5d131136549
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-10-30 10:44:44 +0100

    Record deletion history:Change the menu location

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Record deletion history logs',
-    'version': '1.0.1.1',
+    'version': '1.0.1.2',
     'author':'Soft-integration',
     'category': 'Security/Access rights',
     'description': "",
